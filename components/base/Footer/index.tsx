c93309de7c372fe54df1fd9929bfc01516f72093
Adrian Ardizza <adrian.ardizza@gmail.com>
2021-03-02 20:38:38 +0700

    Adjusted footer on mobile screens

--- components/base/Footer/index.tsx
+++ components/base/Footer/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import tw from 'twin.macro';
+import React from "react"
+import tw from "twin.macro"
 
 const SocialIcon: React.FC<{ src: string }> = ({ src }) => (
   <img tw="w-5 md:w-7 mx-1 md:mx-4" src={src} />
 )
 
 const Footer: React.FC = () => (
-  <div tw="h-24 w-full bg-white">
+  <div tw="h-24 w-full bg-white p-4 md:p-0">
     <div tw="flex flex-col md:flex-row justify-between items-center h-full px-16">
       <div tw="flex flex-row">
-        <img src="/icons/nav/logo_footer.svg" tw="mr-1.5"></img>
+        <img src="/icons/nav/logo_footer.svg" tw="hidden md:block mr-1.5"></img>
         <h1 tw="hidden md:block uppercase text-gray-400 font-bold select-none">
           Kesegaran.Compfest
         </h1>
@@ -27,4 +27,4 @@ const Footer: React.FC = () => (
   </div>
 )
 
-export default Footer;
+export default Footer
