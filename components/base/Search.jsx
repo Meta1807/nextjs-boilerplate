d17b1aa43b8210f2f739cf0726acb1a6bf74da51
Adrian Ardizza <adrian.ardizza@gmail.com>
2021-03-02 20:09:00 +0700

    Added animation for search bar

--- components/base/Search.jsx
+++ components/base/Search.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 import tw from "twin.macro"
+import { motion } from "framer-motion"
 
 const Search = ({ search, searchHandler }) => (
-  <div tw="flex justify-center">
+  <motion.div
+    tw="flex justify-center"
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ duration: 0.5, delay: 0.5, type: "tween" }}
+  >
     <div tw="relative mx-8 w-full md:w-1/4">
       <div tw="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
         <img src="/icons/nav/search.svg" />
@@ -15,7 +21,7 @@ const Search = ({ search, searchHandler }) => (
         placeholder="search for memes ..."
       />
     </div>
-  </div>
+  </motion.div>
 )
 
 export default Search
